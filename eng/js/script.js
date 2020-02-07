7f70318fa8c2d29291ae5667da38a57ef1ba963d
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2020-02-07 20:33:05 +0300

    add full strusture images + adaptive + styles

--- eng/js/script.js
+++ eng/js/script.js
@@ -1,13 +1,48 @@
 /**
  * Define variables
  */
+const mainNode = document.querySelector('.is-app-theme');
+const mainModalNode = document.getElementById('modal-container');
+const modalNodeOther = document.querySelector('.we-modal-1');
+const modalNodeUpdate = document.querySelector('.we-modal-2');
+// const modalCommonNode = document.querySelector('.we-modal');
+// const closeModalButtons = document.querySelectorAll('.close-button');
 const stickNode = document.getElementById('localnav');
 
+/**
+  Show first modal node's definition
+ */
+function showModalFirst() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalNodeOther.classList.remove('modal-container_close');
+}
 
 /**
- * Add styles to nav with scroll
+  Show second modal node's definition
  */
-function ready() {
+function showModalSecond() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalNodeUpdate.classList.remove('modal-container_close');
+}
+
+/**
+ * Close second modal node's definition
+ *
+ * @param {object} currentNode -текущий элемент
+ */
+function closeModal(currentNode) {
+  event.preventDefault();
+  document.body.classList.remove('overflow-hidden');
+  currentNode.closest('.we-modal').classList.add('modal-container_close');
+}
+
+// DOM loaded
+window.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Add styles to nav with scroll
+   */
   window.addEventListener('scroll', function() {
     if (window.pageYOffset >= 44) {
       stickNode.classList.add('localnav-sticking');
@@ -15,9 +50,29 @@ function ready() {
       stickNode.classList.remove('localnav-sticking');
     }
   });
-}
 
-/**
- * Events
+  /**
+  Dilocation and call events
  */
-document.addEventListener('DOMContentLoaded', ready);
+  mainNode.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('button-modal-1')) {
+      showModalFirst();
+    } else if (target.classList.contains('button-modal-2')) {
+      showModalSecond();
+    } else {
+      return;
+    }
+  });
+
+  // Overlay close areas
+  mainModalNode.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('close-button') ||
+        target.classList.contains('overlay')) {
+      closeModal(target);
+    } else {
+      return;
+    }
+  });
+});
