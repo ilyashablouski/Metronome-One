a4867ff01346f11305144f0a00f1f493b23552cb
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2020-02-10 10:46:09 +0300

    add structure for jpn version rus text + jap images

---
+++ jap/js/script.js
@@ -0,0 +1,76 @@
+/**
+ * Define variables
+ */
+const mainNode = document.querySelector('.is-app-theme');
+const mainModalNode = document.getElementById('modal-container');
+const modalNodeOther = document.querySelector('.we-modal-1');
+const modalNodeUpdate = document.querySelector('.we-modal-2');
+const stickNode = document.getElementById('localnav');
+
+/**
+  Show first modal node's definition
+ */
+function showModalFirst() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalNodeOther.classList.remove('modal-container_close');
+}
+
+/**
+  Show second modal node's definition
+ */
+function showModalSecond() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalNodeUpdate.classList.remove('modal-container_close');
+}
+
+/**
+ * Close second modal node's definition
+ *
+ * @param {object} currentNode -текущий элемент
+ */
+function closeModal(currentNode) {
+  event.preventDefault();
+  document.body.classList.remove('overflow-hidden');
+  currentNode.closest('.we-modal').classList.add('modal-container_close');
+}
+
+// DOM loaded
+window.addEventListener('DOMContentLoaded', () => {
+  /**
+   * Add styles to nav with scroll
+   */
+  window.addEventListener('scroll', function() {
+    if (window.pageYOffset >= 44) {
+      stickNode.classList.add('localnav-sticking');
+    } else {
+      stickNode.classList.remove('localnav-sticking');
+    }
+  });
+
+  /**
+  Dilocation and call events
+ */
+  mainNode.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('button-modal-1')) {
+      showModalFirst();
+    } else if (target.classList.contains('button-modal-2')) {
+      showModalSecond();
+    } else {
+      return;
+    }
+  });
+
+  // Overlay close areas
+  mainModalNode.addEventListener('click', (event) => {
+    const target = event.target;
+    if (target.classList.contains('close-button') ||
+        target.classList.contains('overlay')) {
+      closeModal(target);
+    } else {
+      return;
+    }
+  });
+});
