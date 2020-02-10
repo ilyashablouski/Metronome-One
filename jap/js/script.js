325986dd4b0c315c8fb4d73870b5303a7a3250a7
Ilya Shablovskiy <ilya.shablovskiy@gmail.com>
2020-02-10 16:58:58 +0300

    add issues with modals to jap version

--- jap/js/script.js
+++ jap/js/script.js
@@ -5,6 +5,12 @@ const mainNode = document.querySelector('.is-app-theme');
 const mainModalNode = document.getElementById('modal-container');
 const modalNodeOther = document.querySelector('.we-modal-1');
 const modalNodeUpdate = document.querySelector('.we-modal-2');
+const modalUpLeft = document.querySelector('.upLeftModal');
+const modalDownLeft = document.querySelector('.downLeftModal');
+const modalUpRight = document.querySelector('.upRightModal');
+const modalDownRight = document.querySelector('.downRightModal');
+
+
 const stickNode = document.getElementById('localnav');
 
 /**
@@ -26,6 +32,42 @@ function showModalSecond() {
 }
 
 /**
+  Show second modal node's definition
+ */
+function showModalUpLeft() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalUpLeft.classList.remove('modal-container_close');
+}
+
+/**
+  Show second modal node's definition
+ */
+function showModalDownLeft() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalDownLeft.classList.remove('modal-container_close');
+}
+
+/**
+  Show second modal node's definition
+ */
+function showModalUpRight() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalUpRight.classList.remove('modal-container_close');
+}
+
+/**
+  Show second modal node's definition
+ */
+function showModalDownRight() {
+  event.preventDefault();
+  document.body.classList.add('overflow-hidden');
+  modalDownRight.classList.remove('modal-container_close');
+}
+
+/**
  * Close second modal node's definition
  *
  * @param {object} currentNode -текущий элемент
@@ -41,7 +83,7 @@ window.addEventListener('DOMContentLoaded', () => {
   /**
    * Add styles to nav with scroll
    */
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', function () {
     if (window.pageYOffset >= 44) {
       stickNode.classList.add('localnav-sticking');
     } else {
@@ -58,6 +100,14 @@ window.addEventListener('DOMContentLoaded', () => {
       showModalFirst();
     } else if (target.classList.contains('button-modal-2')) {
       showModalSecond();
+    } else if (target.classList.contains('upLeftButton')) {
+      showModalUpLeft();
+    } else if (target.classList.contains('downLeftButton')) {
+      showModalDownLeft();
+    } else if (target.classList.contains('upRightButton')) {
+      showModalUpRight();
+    } else if (target.classList.contains('downRightButton')) {
+      showModalDownRight();
     } else {
       return;
     }
@@ -67,7 +117,7 @@ window.addEventListener('DOMContentLoaded', () => {
   mainModalNode.addEventListener('click', (event) => {
     const target = event.target;
     if (target.classList.contains('close-button') ||
-        target.classList.contains('overlay')) {
+      target.classList.contains('overlay')) {
       closeModal(target);
     } else {
       return;
